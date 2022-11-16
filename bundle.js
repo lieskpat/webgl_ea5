1d67c27aee71e24d19ba418ad04ae71c53db0e24
lies <lies@MacBookvonlies.fritz.box>
2022-11-16 23:01:54 +0100

    Updates

--- bundle.js
+++ bundle.js
@@ -35,6 +35,8 @@ var fragmentGlsl = `
  * Common utilities
  * @module glMatrix
  */
+// Configuration Constants
+var EPSILON = 0.000001;
 var ARRAY_TYPE = typeof Float32Array !== 'undefined' ? Float32Array : Array;
 if (!Math.hypot) Math.hypot = function () {
   var y = 0,
@@ -136,6 +138,86 @@ function identity(out) {
   return out;
 }
 /**
+ * Rotates a mat4 by the given angle around the given axis
+ *
+ * @param {mat4} out the receiving matrix
+ * @param {ReadonlyMat4} a the matrix to rotate
+ * @param {Number} rad the angle to rotate the matrix by
+ * @param {ReadonlyVec3} axis the axis to rotate around
+ * @returns {mat4} out
+ */
+
+function rotate(out, a, rad, axis) {
+  var x = axis[0],
+      y = axis[1],
+      z = axis[2];
+  var len = Math.hypot(x, y, z);
+  var s, c, t;
+  var a00, a01, a02, a03;
+  var a10, a11, a12, a13;
+  var a20, a21, a22, a23;
+  var b00, b01, b02;
+  var b10, b11, b12;
+  var b20, b21, b22;
+
+  if (len < EPSILON) {
+    return null;
+  }
+
+  len = 1 / len;
+  x *= len;
+  y *= len;
+  z *= len;
+  s = Math.sin(rad);
+  c = Math.cos(rad);
+  t = 1 - c;
+  a00 = a[0];
+  a01 = a[1];
+  a02 = a[2];
+  a03 = a[3];
+  a10 = a[4];
+  a11 = a[5];
+  a12 = a[6];
+  a13 = a[7];
+  a20 = a[8];
+  a21 = a[9];
+  a22 = a[10];
+  a23 = a[11]; // Construct the elements of the rotation matrix
+
+  b00 = x * x * t + c;
+  b01 = y * x * t + z * s;
+  b02 = z * x * t - y * s;
+  b10 = x * y * t - z * s;
+  b11 = y * y * t + c;
+  b12 = z * y * t + x * s;
+  b20 = x * z * t + y * s;
+  b21 = y * z * t - x * s;
+  b22 = z * z * t + c; // Perform rotation-specific matrix multiplication
+
+  out[0] = a00 * b00 + a10 * b01 + a20 * b02;
+  out[1] = a01 * b00 + a11 * b01 + a21 * b02;
+  out[2] = a02 * b00 + a12 * b01 + a22 * b02;
+  out[3] = a03 * b00 + a13 * b01 + a23 * b02;
+  out[4] = a00 * b10 + a10 * b11 + a20 * b12;
+  out[5] = a01 * b10 + a11 * b11 + a21 * b12;
+  out[6] = a02 * b10 + a12 * b11 + a22 * b12;
+  out[7] = a03 * b10 + a13 * b11 + a23 * b12;
+  out[8] = a00 * b20 + a10 * b21 + a20 * b22;
+  out[9] = a01 * b20 + a11 * b21 + a21 * b22;
+  out[10] = a02 * b20 + a12 * b21 + a22 * b22;
+  out[11] = a03 * b20 + a13 * b21 + a23 * b22;
+
+  if (a !== out) {
+    // If the source and destination differ, copy the unchanged last row
+    out[12] = a[12];
+    out[13] = a[13];
+    out[14] = a[14];
+    out[15] = a[15];
+  }
+
+  return out;
+}
+/**
  * Generates a orthogonal projection matrix with the given bounds.
  * The near/far clip planes correspond to a normalized device coordinate Z range of [-1, 1],
  * which matches WebGL/OpenGL's clip volume.
@@ -178,6 +260,8 @@ function orthoNO(out, left, right, bottom, top, near, far) {
  */
 
 var ortho = orthoNO;
+
+const geometryModelDatas = [];
 
 function createVertexData() {
     var n = 16;
@@ -256,6 +340,9 @@ function createVertexData() {
         }
     }
 }
+
+geometryModelDatas.push({ description: "torus", function: createVertexData });
+geometryModelDatas.push({ description: "plane", function: createVertexData });
 
 //const gl = initContext("gl_context");
 let gl;
@@ -387,6 +474,7 @@ function initModels() {
     // fill-style
     const fs = "fillwireframe";
     createModel("torus", fs);
+    //createModel("plane", "wireframe");
 }
 
 /**
@@ -398,7 +486,7 @@ function initModels() {
 function createModel(geometryname, fillstyle) {
     const model = {};
     model.fillstyle = fillstyle;
-    initDataAndBuffers(model);
+    initDataAndBuffers(model, geometryname);
     // Create and initialize Model-View-Matrix.
     model.mvMatrix = create();
 
@@ -417,7 +505,11 @@ function initDataAndBuffers(model, geometryname) {
     // vertices, normals, indicesLines, indicesTris;
     // Pointer this refers to the window.
     //this[geometryname]["createVertexData"].apply(model);
-    createVertexData.apply(model);
+    for (let i = 0; i < geometryModelDatas.length; i++) {
+        if (geometryModelDatas[i].description === geometryname) {
+            geometryModelDatas[i].function.apply(model);
+        }
+    }
 
     // Setup position vertex buffer object.
     model.vboPos = gl.createBuffer();
@@ -479,6 +571,7 @@ function render() {
     setProjection();
 
     identity(camera.vMatrix);
+    rotate(camera.vMatrix, camera.vMatrix, (Math.PI * 1) / 4, [1, 0, 0]);
 
     // Loop over models.
     for (let i = 0; i < models.length; i++) {
@@ -487,7 +580,6 @@ function render() {
 
         // Set uniforms for model.
         gl.uniformMatrix4fv(prog.mvMatrixUniform, false, models[i].mvMatrix);
-        console.log(models[i]);
         draw(models[i]);
     }
 }
